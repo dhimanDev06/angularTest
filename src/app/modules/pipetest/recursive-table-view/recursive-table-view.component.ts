763f25dcea6a118f5af8788bee8eba2d9c413a8d
Dhiman <dhiman.bhattacharya@capgemini.com>
2022-07-22 23:11:00 +0530

    recursive code with level

--- src/app/modules/pipetest/recursive-table-view/recursive-table-view.component.ts
+++ src/app/modules/pipetest/recursive-table-view/recursive-table-view.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class RecursiveTableViewComponent implements OnInit {
 
   @Input() data:any;
+  @Input() level:any; 
   constructor() { }
 
   ngOnInit(): void {
--- src/app/modules/pipetest/table-view/table-view.component.ts
+++ src/app/modules/pipetest/table-view/table-view.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TableViewComponent implements OnInit {
 
   constructor() { }
-
+  level:number = 1;
   data:any = {
     title:'main View',
     children:[
