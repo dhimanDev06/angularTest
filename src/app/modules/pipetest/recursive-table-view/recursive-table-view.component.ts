42469010492aae2524aa1a4ea478740c01370e8d
Dhiman <dhiman.bhattacharya@capgemini.com>
2022-07-23 16:34:06 +0530

    working on recursive code

--- src/app/modules/pipetest/recursive-table-view/recursive-table-view.component.ts
+++ src/app/modules/pipetest/recursive-table-view/recursive-table-view.component.ts
@@ -1,17 +1,32 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-recursive-table-view',
   templateUrl: './recursive-table-view.component.html',
   styleUrls: ['./recursive-table-view.component.css']
 })
-export class RecursiveTableViewComponent implements OnInit {
+export class RecursiveTableViewComponent implements OnInit, OnChanges {
 
   @Input() data:any;
   @Input() level:any; 
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.data && this.data.children && this.data.children.length > 0 ){
+      this.data['show'] = false;
+    }
+  }
 
   ngOnInit(): void {
+  }
+
+  check(d:any){
+    console.log('check',d, d.show);
+    if(d.show == true){
+      d.show = false
+    }else{
+      d.show = true
+    }
+    
   }
 
 }
